3ad16bfd18d1e3a34ac2178d8bc090d066c171d5
Rafael Oliveira <rafdev.x@gmail.com>
2021-12-04 00:50:29 +0000

    day 3 2021 part 2

--- 2021/day03/part2.cpp
+++ 2021/day03/part2.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -5,20 +8,33 @@
 
 using namespace std;
 
-int most_common(vector<vector<int>> count, bool least_common = false)
+bool most_common_msb(vector<int> nums, unsigned int binLen)
 {
-	string num = "";
-	for (int i = 0; i < count.size(); i++) {
-		char n = '0';
-		if (count[i][1] > count[i][0]) {
-			n = '1';
+	int zeroCount = 0;
+	int sz = nums.size();
+
+	for (unsigned int i = 0; i < sz; i++) {
+		if ((nums[i] & 1 << (binLen - 1)) == 0) {
+			zeroCount++;
 		}
-		if (least_common) {
-			n = n == '1' ? '0' : '1';
-		}
-		num += n;
 	}
-	return stoi(num, nullptr, 2);
+
+	return zeroCount <= sz / 2;
+}
+
+int most_common_recursive_filter(vector<int> nums, unsigned int binLen, bool leastCommon = false)
+{
+	bool most = most_common_msb(nums, binLen);
+	vector<int> filtered;
+	copy_if(nums.begin(), nums.end(), back_inserter(filtered), [=](int i) {
+		bool val = min((int)(i & 1 << (binLen - 1)), 1) == most;
+		return leastCommon ? !val : val;
+	});
+	if (filtered.size() <= 1) {
+		return filtered[0];
+	} else {
+		return most_common_recursive_filter(filtered, binLen - 1, leastCommon);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -31,22 +47,17 @@ int main(int argc, char *argv[])
 	file.open(argv[1], ios::in);
 	if (file.is_open()) {
 		string line;
-		vector<vector<int>> count;
-		bool firstLine = true;
+		vector<int> nums;
+		unsigned int binLen = 0;
 		while (getline(file, line)) {
-			for (unsigned int i = 0; i < line.length(); i++) {
-				int bit = line[i] == '1' ? 1 : 0;
-				if (firstLine) {
-					count.push_back(vector<int>(2, 0));
-				}
-				count[i][bit]++;
-			}
-			firstLine = false;
+			binLen = line.size();
+			nums.push_back(stoi(line, nullptr, 2));
 		}
-		int gamma_rate = most_common(count);
-		int epsilon_rate = most_common(count, true);
-		cout << "Gamma Rate: " << gamma_rate << ", Epsilon Rate: " << epsilon_rate << endl;
-		cout << "Power Consumption: " << gamma_rate * epsilon_rate << endl;
+		int og_rating = most_common_recursive_filter(nums, binLen);
+		int cs_rating = most_common_recursive_filter(nums, binLen, true);
+		cout << "Oxygen Generator Rating: " << og_rating << endl;
+		cout << "CO2 Scrubber Rating: " << cs_rating << endl;
+		cout << "Power Consumption: " << og_rating * cs_rating << endl;
 	}
 	return 0;
 }
