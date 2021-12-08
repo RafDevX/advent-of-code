57b7fdf3d13cd05ae969d0f1dca0b59ed6f3d458
Rafael Oliveira <rafdev.x@gmail.com>
2021-12-08 02:53:14 +0000

    day 7 2021 part 2

---
+++ 2021/day07/part2.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int calculate_fuel_cost(vector<int> crabs, int alignAt)
+{
+	int cost = 0;
+	for (int i = 0; i < crabs.size(); i++) {
+		for (int j = 1; j <= abs(crabs[i] - alignAt); j++) {
+			cost += j;
+		}
+	}
+	return cost;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		cout << "Run with text file as argument" << endl;
+		return 1;
+	}
+	fstream file;
+	file.open(argv[1], ios::in);
+	if (file.is_open()) {
+		vector<int> crabs;
+		int minPos, maxPos;
+		bool first = true;
+		for (int i; file >> i;) {
+			crabs.push_back(i);
+			if (first || i < minPos) {
+				minPos = i;
+			}
+			if (first || i > maxPos) {
+				maxPos = i;
+			}
+			first = false;
+			if (file.peek() == ',') {
+				file.ignore();
+			}
+		}
+		cout << "Brute-forcing in [" << minPos << ", " << maxPos << "]\n";
+		first = true;
+		int minFuel, alignmentPos;
+		for (int pos = minPos; pos <= maxPos; pos++) {
+			int fuel = calculate_fuel_cost(crabs, pos);
+			if (first || fuel < minFuel) {
+				minFuel = fuel;
+				alignmentPos = pos;
+			}
+			first = false;
+		}
+		cout << "Minimum fuel spent is " << minFuel << " when aligning at position " << alignmentPos << "\n";
+	}
+	return 0;
+}
